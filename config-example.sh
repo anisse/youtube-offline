8d7e0b894ed2e5f57f5c33f2ba7d0c4b67cbb144
Anisse Astier <anisse@astier.eu>
2013-06-06 23:36:07 +0200

    Initial commit

---
+++ config-example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ID of your publicly accessible playlist
+PLAYLIST_ID="PLSTz8jpJdr5pkXfNu3IQAOYIQjjTY0DMj"
+DEST_DIR="$HOME/data/youtube-offline"
+
+FTP_HOST="192.168.1.254"
+FTP_LOGIN="boitegratuite"
+FTP_PASSWORD="verylongandcomplicatedpassword"
+FTP_REMOTE_DIR="/HD/Videos/Youtube offline"
+
